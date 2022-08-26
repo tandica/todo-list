3ba52b173e1fca8cf07a9844fc0345726c6162c1
tandica <williams.tandica@gmail.com>
2022-08-25 22:03:12 -0400

    step 8 - create checked functionality and style it

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 /* create a close button and append it to each item 
 this allows us to delete items in the list */
-const myList = document.getElementsByTagName("li");
+const myList = document.getElementsByTagName("LI");
 
 /* loops are used when you want to run the same code over again
 here we are going through each list item by accessing the elements through getElementsByTagName above */
 for (let i = 0; i < myList.length; i++) {
   //create a span for each list item
-  const span = document.createElement("span");
+  const span = document.createElement("SPAN");
   //this is the "x" symbol
   //createTextNode creates a text string from the specified value
   const symbol = document.createTextNode("\u00D7");
@@ -26,3 +26,20 @@ for (let i = 0; i < remove.length; i++) {
     hide.style.display = "none";
   };
 }
+
+/* add a "checked" marker when clicking on a list item */
+const list = document.querySelector("ul");
+
+//the eventListener fires when a user clicks on something
+list.addEventListener(
+  "click",
+  function (event) {
+    //we want to target the LI tag
+    if (event.target.tagName === "LI") {
+      //classList accesses the class attributes of an element
+      //we can create a new class that has distinctive styling named "checked"
+      event.target.classList.toggle("checked");
+    }
+  },
+  false
+);
