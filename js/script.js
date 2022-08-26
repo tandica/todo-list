fe883f7c7be5abf539af5a050a7519c500d1792f
tandica <williams.tandica@gmail.com>
2022-08-25 22:35:51 -0400

    step 9 - create functionality for adding a new list item

--- js/script.js
+++ js/script.js
@@ -43,3 +43,35 @@ list.addEventListener(
   },
   false
 );
+
+function newElement() {
+  //make new list element
+  const task = document.createElement("li");
+  //access the value of the input field
+  const inputValue = document.getElementById("my-input").value;
+  const addItem = document.createTextNode(inputValue);
+  task.appendChild(addItem);
+
+  //create alert for when input is empty and user clicks on add a task button
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    //this is getting the data from line 54 and adding it to the list through id
+    document.getElementById("my-list").appendChild(task);
+  }
+  document.getElementById("my-input").value = "";
+
+  //add the deleting functionality to the new list item
+  const span = document.createElement("SPAN");
+  const symbol = document.createTextNode("\u00D7");
+  span.className = "delete";
+  span.appendChild(symbol);
+  task.appendChild(span);
+
+  for (i = 0; i < remove.length; i++) {
+    remove[i].onclick = function () {
+      const hide = this.parentElement;
+      hide.style.display = "none";
+    };
+  }
+}
